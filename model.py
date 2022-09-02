3efc2d460d0185acf1507d6d41b14ae53a051583
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2022-09-02 21:42:03 +0530

    added padding

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 class SegregationModel:
 	def __init__(self, similarity=3, red_blue_ratio=0.5, empty=0.1, side=50):
-		self.similarity = similarity
+		self.similarity = similarity/8
 		self.red_blue_ratio = red_blue_ratio
 		self.empty = empty
 		self.side = side
-		self.map = np.zeros((self.side, self.side))
+		self.map = np.zeros((self.side+2, self.side+2))
 		self.randomly_populate()
 
 	def randomly_populate(self):
@@ -14,7 +15,26 @@ class SegregationModel:
 		number_of_empties = int(total*self.empty)
 		number_of_blues = int(self.red_blue_ratio*(total - number_of_empties))
 		number_of_reds = (total - number_of_empties) - number_of_blues
-		print(total, number_of_empties, number_of_blues, number_of_reds)
+		relevant_indices = np.random.choice(np.arange(total), size=number_of_blues+number_of_reds, replace=False)
+		reds = [(i%self.side, i//self.side) for i in relevant_indices[:number_of_reds]]
+		blues = [(i%self.side, i//self.side) for i in relevant_indices[number_of_reds:]]
+		for (i,j) in reds:
+			self.map[i+1, j+1] = 1
+		for (i,j) in blues:
+			self.map[i+1, j+1] = -1
+		plt.imshow(self.map)
+		plt.show()
+
+	def get_similar_units(self, i, j, val):
+		pass
+
+
+	def run_single_simulation(self):
+		for i,row in enumerate(self.map):
+			for j,col in enumerate(row):
+				n = self.get_similar_units(i, j, col)
+
 
 if __name__ == '__main__':
 	sm = SegregationModel()
+	sm.run_single_simulation()
